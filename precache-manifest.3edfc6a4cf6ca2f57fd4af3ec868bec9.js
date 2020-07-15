375eb15159a8d25de42f6753c20e04c1e67830f8
LIFE <dkrshw65@gmail.com>
2020-07-15 07:41:22 -0400

    Updates

---
+++ precache-manifest.3edfc6a4cf6ca2f57fd4af3ec868bec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc56b0b77e5b1082b32aedd3b9e06436",
+    "url": "./index.html"
+  },
+  {
+    "revision": "429d1399e0a31ecd35c1",
+    "url": "./static/css/main.ce9396f4.chunk.css"
+  },
+  {
+    "revision": "667a98c71978065af651",
+    "url": "./static/js/2.211e754d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.211e754d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429d1399e0a31ecd35c1",
+    "url": "./static/js/main.90916499.chunk.js"
+  },
+  {
+    "revision": "85928b8ea9d7c71bbb97",
+    "url": "./static/js/runtime-main.b3f0dd32.js"
+  }
+]);
